91596485d3d42e4063beb580ecfb5f477d9a3b75
piotrmatusik <piotrmatusik2001@o2.pl>
2018-04-08 10:25:27 +0200

    initial commit

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'piotrmatusik2001@gmail.com'
+  layout 'mailer'
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path 
+    assert_response :success
+    assert_select "title", "Ruby On Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | Ruby On Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Ruby On Rails Tutorial Sample App"
+  end
+  test "should get contact" do 
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby On Rails Tutorial Sample App"
+end
+end
